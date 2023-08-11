dada72adbd729c4fdd811588434559ade0a5dbd8
Laboratorio Estudiantes <javiviba13@gmail.com>
2023-08-10 20:40:13 -0600

    se establecio correctamente la base del proyecto

--- API_Introduccion_Redes/Capa_Controller/Controllers/EjemploController.cs
+++ API_Introduccion_Redes/Capa_Controller/Controllers/EjemploController.cs
@@ -1,5 +1,6 @@
 ﻿using Capa_Logica;
 using Capa_Logica.OrquestadorClientes;
+using Capa_Logica.OrquestadorUsuarios;
 using Capa_Modelo.Cliente;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -10,47 +11,7 @@ namespace Capa_Controller.Controllers
     [Route("[Controller]")]
     public class EjemploController : Controller
     {
-        private OrquestadorClientes orquestador = new OrquestadorClientes();
-        [HttpGet]
-        [Route("ObtengaClientesDisponibles")]
-        public IActionResult ObtengaClientesDisponibles() {
-            List<Cliente> clientes = orquestador.ObtengaClientes();
-            return Ok(clientes);
-        }
+        private OrquestadorUsuarios orquestador = new OrquestadorUsuarios();
 
-        [HttpPost]
-        [Route("AgregueCliente")]
-        public IActionResult AgregueCliente(Cliente cliente) {
-             bool resultado = orquestador.AgregueCliente(cliente);
-            if (resultado)
-            {
-                return Ok();
-            }
-            else {
-                return BadRequest();
-            }
-        }
-        [HttpPost]
-        [Route("Login")]
-        public IActionResult Login(string nombre, string identificacion) {
-
-            
-            bool resultado = orquestador.Login(nombre, identificacion);
-            return resultado ? Ok() : Unauthorized();
-        }
-
-        [HttpPut]
-        [Route("ActualiceCliente")]
-        public IActionResult ActualiceCliente(string identificacion, string apellido2) {
-            bool resultado = orquestador.ActualiceCliente(identificacion, apellido2);
-            return resultado ? Ok() : BadRequest();
-        }
-
-        [HttpDelete]
-        [Route("BorreCliente")]
-        public IActionResult BorreCliente(string identificacion) {
-            bool resultado = orquestador.BorreCLiente(identificacion);
-            return resultado ? Ok() : BadRequest();
-        }
     }
 }
